013621cf2d4bc0a86f13fbf531e3b0befce9f8a7
John Helder <johnhelderjp@gmail.com>
2022-11-06 10:42:39 -0300

    Concluindo as funcionalidades do jogo

--- script.js
+++ script.js
@@ -1,54 +1,113 @@
-// Initial Data
-let square = {
+// Dados iniciais
+let quadro = {
   a1: '', a2: '', a3: '',
   b1: '', b2: '', b3: '',
-  c1: '', c2:'', c3: ''
+  c1: '', c2: '', c3: ''
 };
-
-let player = '';
+let playing = false;
+let vez = 'x';
 let warning = '';
-let playing = '';
 
 reset();
 
-//Events
-  document.querySelector(".reset").addEventListener("click", reset);
-  document.querySelector(".item").forEach(item => {
-    item.addEventListener("click", itemClick);
+// Eventos
+document.querySelector('.reset').addEventListener('click', reset);
+
+document.querySelectorAll('.item').forEach((item)=>{
+  item.addEventListener('click', (e) => {
+      let loc = e.target.getAttribute('data-item');
+      
+      if(playing && quadro[loc] === '') {
+          quadro[loc] = vez;
+          renderQuadro();
+          togglePlayer();
+      }
   });
+});
 
-//Functions
-function itemClick(){
-  let item = event.target.getAttribute("data-item");
-  if(square[item] === ""){
-    square[item] = player;
-    renderSquare();
-  }
-}
-
-function reset(){
+// Funções
+function reset() {
   warning = '';
-  let random = Math.floor(Math.random() * 2);
-  player = (random === 0) ? "x" : "o";
 
-  for(let i in square){
-    square[i] = '';
+  // definir a vez
+  let random = Math.floor(Math.random() * 2);
+  vez = random === 0 ? 'x' : 'o';
+
+  // resetar os quadros
+  for(let i in quadro) {
+      quadro[i] = '';
   }
+
+  // renderizar tudo
+  renderQuadro();
+  renderInfo();
 
   playing = true;
-
-  renderSquare();
-  renderInfor();
 }
 
-function renderSquare(){
-  for(let i in square){
-    let item = document.querySelector(`div[data-item=${i}]`);
-    item.innerHTML = square[i];
+function renderQuadro() {
+  for(let i in quadro) {
+      let item = document.querySelector(`div[data-item=${i}]`);
+      if(quadro[i] !== '') {
+          item.innerHTML = quadro[i];
+      } else {
+          item.innerHTML = '';
+      }
+  }
+
+  checkGame();
+}
+
+function renderInfo() {
+  document.querySelector('.vez').innerHTML = vez;
+  document.querySelector('.resultado').innerHTML = warning;
+}
+
+function togglePlayer() {
+  vez = vez === 'x' ? 'o' : 'x';
+  renderInfo();
+}
+
+function checkGame() {
+  if(checkWinnerFor('x')) {
+      warning = 'O "x" venceu';
+      playing = false;
+  } else if(checkWinnerFor('o')) {
+      warning = 'O "o" venceu';
+      playing = false;
+  } else if(isFull()) {
+      warning = 'Deu empate';
+      playing = false;
   }
 }
 
-function renderInfor(){
-  document.querySelector('.vez').innerHTML = player;
-  document.querySelector('.resultado').innerHTML = warning;
+function checkWinnerFor(i) {
+  let pos = [
+      'a1,a2,a3',
+      'b1,b2,b3',
+      'c1,c2,c3',
+
+      'a1,b1,c1',
+      'a2,b2,c2',
+      'a3,b3,c3',
+
+      'a1,b2,c3',
+      'a3,b2,c1'
+  ];
+
+  for(let w in pos) {
+      let pArray = pos[w].split(',');
+      let hasWon = pArray.every(option=>quadro[option] === i);
+      if(hasWon) return true;
+  }
+
+  return false;
+}
+function isFull() {
+  for(let i in quadro) {
+      if(quadro[i] === '') {
+          return false;
+      }
+  }
+  return true;
 }
