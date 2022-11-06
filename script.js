f82b7c2de5d6eae20320bd49a67c16d02903e781
John Helder <johnhelderjp@gmail.com>
2022-11-06 09:54:18 -0300

    Função para percorrer itens e adicionar evento

--- script.js
+++ script.js
@@ -13,6 +13,9 @@ reset();
 
 //Events
   document.querySelector(".reset").addEventListener("click", reset);
+  document.querySelector(".item").forEach(item => {
+    item.addEventListener("click", itemClick);
+  });
 
 //Functions
 function reset(){
